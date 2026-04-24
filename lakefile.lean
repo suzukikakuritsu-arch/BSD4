195855e6f014d3032e7cc8ce533031678dbf7dd0
Yukiya Suzuki <suzuki.kakuritsu@gmail.com>
2026-04-25 04:43:19 +0900

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -1,10 +1,14 @@
 import Lake
 open Lake DSL
 
-package bsd
+package bsd {
+  -- プロジェクト設定
+}
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
 @[default_target]
-lean_lib BSD
+lean_lib BSD {
+  -- ライブラリ設定
+}
